084ebd234f48c35f66d94436a5597c5a956f4102
pranav <pranavbavaskar1@gmail.com>
2025-04-04 21:21:55 +0530

    added next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // This allows any domain
+        port: "", // Empty means any port
+        pathname: "**", // Allows any path
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Exclude problematic modules from client-side bundles
+      config.externals.push("clone-deep");
+    }
+    return config;
+  },
+
 };
 
 export default nextConfig;
