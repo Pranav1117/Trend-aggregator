900c9368c0be3007c3356e4bcb75c4e968372fc5
pranav <pranavbavaskar1@gmail.com>
2025-04-09 20:11:16 +0530

    testing response on vercel

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -3,11 +3,11 @@ import { fetchYouTubeTrends } from "@/lib/api/youtube";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  console.log("YT env var", process.env.YOUTUBE_API_KEY);
   const { searchParams } = new URL(request.url);
   const searchQuery: string | null = searchParams.get("q");
-
+  
   const YTResponse = await fetchYouTubeTrends(searchQuery);
+  console.log("YT respoinse ------------", YTResponse);
   const redditResponse = await fetchRedditTrends(searchQuery);
 
   const response = [...YTResponse, ...redditResponse];
